2c2ac100d35db1b99afdec4246436e0a0cf1e57f
Samuel Agyekum <agyekumsamuel055@gmail.com>
2024-09-15 00:14:18 +0000

    Add basic arithmetic in the code

--- basic_operations.py
+++ basic_operations.py
@@ -2,11 +2,11 @@ number1 = 10
 number2 = 5
 
 sum_result = number1 + number2
-print("Addition of {} and {} is {}".format(number1, number2, sum_result))
-
-
 difference_result = number1 - number2
-print("Subtraction of {} and {} is {}".format(number1, number2, difference_result))
-
 product_result = number1 * number2
-print("Multiplication of {} and {} is {}".format(number1, number2, product_result))
+
+print(f"Sum of {number1} and {number2} is {sum_result}. ")
+print(f"Difference of {number1} and {number2} is {difference_result}. ")
+print(f"Product of {number1} and {number2} is {product_result}. ")
+
+
